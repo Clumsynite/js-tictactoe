3db704f6daa3a3223353beab526cb845e7a6514c
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-08-02 17:18:19 +0530

    Added input validation to get player names

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const gameBoard = document.querySelector('#game-board')
 const cells = document.querySelectorAll('.cells')
+const getPlayerNames = document.querySelector('#get-player')
+const inputOne = document.querySelector('#player-1')
+const inputTwo = document.querySelector('#player-2')
+const startBtn = document.querySelector('#start')
+
+const emptyWarning = field => {
+  field.value = 'Enter something here'
+  setTimeout(() => {
+    field.value = ''
+  }, 1000)
+}
+const startGame = event => {
+  if(inputOne.value === ''){emptyWarning(inputOne);}
+  else if(inputTwo.value === ''){emptyWarning(inputTwo)}
+  else { 
+    getPlayerNames.style.display = 'none'
+    gameBoard.style.display = 'grid'
+  }
+}
+
+startBtn.addEventListener('click', startGame)
+
 
 const cellHover = event => {
   const cellId = event.target.getAttribute('data-cell') 
@@ -10,3 +32,6 @@ Array.from(cells).forEach(cell => {
   cell.addEventListener('mouseover', cellHover)
 })
 
+const player = (name, selection) => {
+  return {name, selection}
+}
