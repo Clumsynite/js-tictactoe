61f00c58df30233bd46ce19cd1c765224fb31395
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-08-07 19:53:41 +0530

    Added Player vs Computer mode
    Needs refactoring, delay in move placement
    minimax not implemented
    random placement used

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const heading = document.querySelector('#heading')
+const gameMode = document.querySelector('#game-mode')
+const vsPlayer = document.querySelector('#vs-player')
+const vsPc = document.querySelector('#vs-pc')
 const getPlayerNames = document.querySelector('#get-player')
 const inputOne = document.querySelector('#player-1')
 const inputTwo = document.querySelector('#player-2')
-const startBtn = document.querySelector('#start')
+const startBtn = document.querySelector('#start-pvp')
+const compDiv = document.querySelector('#get-x')
+const getPlayerO = document.querySelector('#plr-x')
+const inputX = document.querySelector('#player-x')
+const startComp = document.querySelector('#start-pvc')
 const nowPlaying = document.querySelector('#now-playing')
 const gameBoard = document.querySelector('#game-board')
 const cells = document.querySelectorAll('.cells')
@@ -19,6 +26,17 @@ const Gameboard = (() => {
   let board = { 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '',9: ''}
 
   const getBoard = () => board
+  
+  const getCell = (cell) => {
+    return board[cell]
+  }
+
+  const checkBoardFull = () => {
+    if(Object.values(board).indexOf('')===-1){
+      return true
+    }
+    return false
+  }
 
   const renderBoard =() => {
     Array.from(cells).forEach( cell => {
@@ -87,7 +105,7 @@ const Gameboard = (() => {
     }
   }
 
-  return {clearBoard, getBoard, renderBoard, placeMove, preventOverwrite, checkWin, disableBoard, enableBoard, checkTie}
+  return {clearBoard, getBoard, getCell, checkBoardFull, renderBoard, placeMove, preventOverwrite, checkWin, disableBoard, enableBoard, checkTie}
 })();
 
 const nameMethods = (() => {
@@ -114,6 +132,7 @@ const nameMethods = (() => {
 })();
 
 const playerMethods = (() => {
+  let mode = ''
   const switchTurns = () => {
     if(playerOne.turn){
       playerOne.turn = false
@@ -132,7 +151,17 @@ const playerMethods = (() => {
   return {switchTurns, getCurrentPlayer, logTurn}
 })();
 
-const initialisePlayer = () => {
+const twoPlayerInput = () => {
+  gameMode.style.display = 'none'
+  getPlayerNames.style.display = 'flex'
+}
+
+const onePlayerInput = () => {
+  gameMode.style.display = 'none'
+  compDiv.style.display = 'flex'
+}
+
+const initialiseTwoPlayer = () => {
   heading.innerHTML = '<strong>Tic Tac Toe</strong>'
   getPlayerNames.style.display = 'none'
   gameBoard.style.display = 'grid'
@@ -140,34 +169,87 @@ const initialisePlayer = () => {
   playerOne = player(inputOne.value, 'X', true)
   playerTwo = player(inputTwo.value, 'O', false)
   nowPlaying.innerHTML = `Player <strong>X</strong> is <strong>${playerOne.name}</strong> and Player <strong>O</strong> is <strong>${playerTwo.name}</strong>`
+  playerMethods.mode = 'pvp'
   setTimeout(() => {
     playerMethods.logTurn()
-  }, 2000)
+  }, 1000)
 }
-
-const startGame = event => {
+const initialiseOnePlayer = () => {
+  heading.innerHTML = '<strong>Tic Tac Toe</strong>'
+  compDiv.style.display = 'none'
+  gameBoard.style.display = 'grid'
+  buttons.style.display = 'block'
+  playerOne = player(inputX.value, 'X', true)
+  playerTwo = player('Computer', 'O', false)
+  nowPlaying.innerHTML = `Player <strong>X</strong> is <strong>${playerOne.name}</strong> and Player <strong>O</strong> is <strong>${playerTwo.name}</strong>`
+  playerMethods.mode = 'pvc'
+  setTimeout(() => {
+    playerMethods.logTurn()
+  }, 1000)
+}
+const startGame = () => {
   if(inputOne.value.trim()==='' ){nameMethods.emptyWarning(inputOne);}
   else if(inputOne.value.length > 12){nameMethods.lengthWarning(inputOne)}
   else if(inputTwo.value.trim()===''){nameMethods.emptyWarning(inputTwo)}
   else if(inputTwo.value.length > 12){nameMethods.lengthWarning(inputTwo)}
   else if(inputOne.value===inputTwo.value){nameMethods.sameNameWarning()}
   else { 
-    initialisePlayer()
+    initialiseTwoPlayer()
   }
+}
+const compGame = () => {
+  if(inputX.value.trim()===''){nameMethods.emptyWarning(inputX);}
+  else if(inputX.value.length > 12){nameMethods.lengthWarning(inputX)}
+  else {
+    initialiseOnePlayer()
+  }
+}
+
+const twoPlayerGame = () => {
+  const cellId = event.target.getAttribute('data-cell')
+  const playerSelection = playerMethods.getCurrentPlayer().selection
+  Gameboard.placeMove(cellId, playerSelection)
+  Gameboard.renderBoard()
+  playerMethods.switchTurns()
+  playerMethods.logTurn()
+  Gameboard.checkTie()
+  Gameboard.checkWin()
+}
+
+const onePlayerGame = () => {
+  const cellId = event.target.getAttribute('data-cell')
+  const playerSelection = playerMethods.getCurrentPlayer().selection
+  Gameboard.placeMove(cellId, playerSelection)
+  playerMethods.switchTurns()
+  computerPlays()
+  Gameboard.renderBoard()
+  Gameboard.checkTie()
+  Gameboard.checkWin()
+
+}
+
+const computerPlays = () => {
+  const playerSelection = playerMethods.getCurrentPlayer().selection
+  let random = Math.floor(Math.random()* 8) + 1
+  while(Gameboard.getCell(random) !== ''){
+    random = Math.floor(Math.random()* 8) + 1
+    if(Gameboard.checkBoardFull()){
+      break;
+    }
+  }
+  Gameboard.placeMove(random, playerSelection)
+  Gameboard.checkTie()
+  Gameboard.checkWin()
+  playerMethods.switchTurns()
 }
 
 const onClick = () => {
   if (event.target.textContent !== ''){
     Gameboard.preventOverwrite()
-  }else {
-    const cellId = event.target.getAttribute('data-cell')
-    const playerSelection = playerMethods.getCurrentPlayer().selection
-    Gameboard.placeMove(cellId, playerSelection)
-    Gameboard.renderBoard()
-    playerMethods.switchTurns()
-    playerMethods.logTurn()
-    Gameboard.checkTie()
-    Gameboard.checkWin()
+  }else if(playerMethods.mode ==='pvp'){
+    twoPlayerGame()
+  }else if(playerMethods.mode ==='pvc'){
+    onePlayerGame()
   }
 }
 
@@ -181,9 +263,13 @@ const resetGame = () => {
   Gameboard.enableBoard()
 }
 
-startBtn.addEventListener('click', startGame)
+vsPlayer.addEventListener('click', twoPlayerInput)
+vsPc.addEventListener('click', onePlayerInput)
 
-Array.from(cells).forEach(cell => {
+startBtn.addEventListener('click', startGame)
+startComp.addEventListener('click', compGame)
+
+cells.forEach(cell => {
   cell.addEventListener('click', onClick)
 })
 
